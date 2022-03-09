89d8212df0fd9372fb8eca10e68dd685d2cea934
Alexgv1108 <alexgallego1108@gmail.com>
2022-03-08 20:24:44 -0500

    Se crea redirección en la url para el 404 not found

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 
 import Login from './modules/Login';
+import NotFound from './components/NotFound';
 
 const App = () => {
   return (
     <Router> 
       <Routes>
         <Route exact path="/" element={<Login />} asename={process.env.PUBLIC_URL}/>
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
   )
