b5a59d2cd9eb23af813f02a1e841adc73de55f89
Alexgv1108 <alexgallego1108@gmail.com>
2024-11-23 18:29:13 -0500

    Fix: Se soluciona error del scroll y asincronismo con la búsqueda de resultados al estar en la posición 0

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -2,7 +2,10 @@ import './loader.css'
 
 export const Loader = () => {
   return (
-    <div className="d-flex justify-content-center align-items-center" style={{ height: "100vh" }}>
+    <div className="d-flex justify-content-center align-items-center" style={{ 
+        height: "100vh",
+        zIndex: '9999'
+      }}>
       <div>
         <div className="spinner"></div>
         <p className="text-secondary">Cargando...</p>
--- src/pages/chat/Chat.jsx
+++ src/pages/chat/Chat.jsx
@@ -11,6 +11,8 @@ const db = getDatabase();
 const fechaInicioPagina = new Date();
 let usuarioSesion = {};
 let ultimaFechaPaginacion = null;
+let isCargaCompleta = false;
+let todosLosRegistrosConsultados = false;
 
 export const Chat = () => {
 
@@ -20,12 +22,12 @@ export const Chat = () => {
     const chatEndRef = useRef();
 
     const [posts, setPosts] = useState([]);
-    const [acumPost, setAcumPost] = useState([])
     const [usuarios, setUsuarios] = useState([]);
     const [uidChat, setUidChat] = useState('');
     const [showUsers, setShowUsers] = useState(false);
     const [loading, setLoading] = useState(true);
     const [pagination, setPagination] = useState(null);
+    const [heighPosicionChat, setHeighPosicionChat] = useState(0);
 
     // Listener para detectar sesión activa
     useEffect(() => {
@@ -60,34 +62,11 @@ export const Chat = () => {
         }
     }
 
-    // Consulta de mensajes al seleccionar un chat
-    useEffect(() => {
-        if (!usuarioSesion.uid || !uidChat) return;
-        const consultaAsync = async () => {
-            const pathTo = `${uidChat}/${usuarioSesion.uid}`;
-            const pathFrom = `${usuarioSesion.uid}/${uidChat}`;
-            const response = await getFullMessageByPath(db, pathFrom, pathTo, pagination);
-            const responseArray = Object.entries(response);
-
-            // ordena por fecha
-            const responseArrayOrder = responseArray.sort((a, b) => new Date(a[1].fecha) - new Date(b[1].fecha));
-            if (chatStartRef.current?.scrollTop === 0 && (!ultimaFechaPaginacion || ultimaFechaPaginacion !== responseArrayOrder[0][1].fecha)) {
-                ultimaFechaPaginacion = responseArrayOrder[0][1].fecha;
-                setAcumPost(acomuladorPost => [...acomuladorPost, ...responseArrayOrder]);
-                setPagination(ultimaFechaPaginacion);
-                return;
-
-                // PENDIENTE CONTROLAR CANTIDAD DE PETICIONES
-            }
-            setPosts(postState => [...acumPost, ...responseArrayOrder, ...postState]);
-            setAcumPost([]);
-        }
-        consultaAsync();
-    }, [uidChat, pagination]);
-
     // Listener en tiempo real para mensaje
     useEffect(() => {
         if (!uidChat || !usuarioSesion.uid) return;
+        isCargaCompleta = false;
+        todosLosRegistrosConsultados = false;
         const postsRef = query(ref(db, `/chat/${uidChat}/${usuarioSesion.uid}`), limitToLast(1));
         const unsubscribe = onChildAdded(postsRef, (snapshot) => {
             const newMessage = snapshot.val();
@@ -100,24 +79,62 @@ export const Chat = () => {
         return () => unsubscribe()
     }, [uidChat]);
 
+    // Consulta de mensajes al seleccionar un chat
+    useEffect(() => {
+        if (!usuarioSesion.uid || !uidChat || todosLosRegistrosConsultados) return;
+        (async () => {
+            setLoading(true);
+            const pathTo = `${uidChat}/${usuarioSesion.uid}`;
+            const pathFrom = `${usuarioSesion.uid}/${uidChat}`;
+            const response = await getFullMessageByPath(db, pathFrom, pathTo, pagination ? pagination : null);
+            const responseArray = Object.entries(response);
+
+            if (!responseArray || !responseArray.length) {
+                todosLosRegistrosConsultados = true;
+                setLoading(false);
+                return;
+            }
+            // ordena por fecha
+            const dataSort = responseArray.sort((a, b) => new Date(a[1].fecha) - new Date(b[1].fecha));
+            const lengArray = dataSort.length;
+            const responseArrayMitad = [...dataSort].slice((lengArray / 2) - 1, lengArray - 1)
+            setPosts(postState => [...responseArrayMitad, ...postState]);
+            setLoading(false);
+        })();
+    }, [uidChat, pagination]);
+
+    useEffect(() => {
+        if (!chatStartRef.current || !heighPosicionChat || !isCargaCompleta || loading) return;
+        debugger;
+        chatStartRef.current.scrollTo({
+            top: heighPosicionChat
+        });
+
+    }, [posts]);
+
     // Mueve el scroll hacia abajo
     useEffect(() => {
-        if (posts && chatEndRef.current) {
-            chatEndRef.current.scrollIntoView({
-                block: 'end',
-            });
-        }
+        if (loading || !posts.length || !chatEndRef.current || isCargaCompleta || todosLosRegistrosConsultados) return;
+        if (!isCargaCompleta) scrollAbajo();
+        if (chatStartRef.current?.scrollTop === 0) {
+            if (ultimaFechaPaginacion === posts[0][1].fecha) return;
+            ultimaFechaPaginacion = posts[0][1].fecha;
+            setPagination(ultimaFechaPaginacion);
+        } else isCargaCompleta = true
     }, [posts]);
 
     const handleScroll = () => {
         const position = chatStartRef.current?.scrollTop;
         if (position === 0) {
-            setPagination(ultimaFechaPaginacion);
+            setPagination(posts[0][1].fecha);
+            const { clientHeight } = chatStartRef.current;
+            setHeighPosicionChat(clientHeight);
         }
     };
 
+    // Evento para cuando se mueve el mouse
     useEffect(() => {
-        if (!uidChat || !posts) return;
+        if (!uidChat || !posts || loading) return;
         const container = chatStartRef.current;
         if (container) {
             container.addEventListener('scroll', handleScroll);
@@ -128,53 +145,59 @@ export const Chat = () => {
                 container.removeEventListener('scroll', handleScroll);
             }
         };
-    }, [uidChat]);
+    }, [posts]);
+
+    const scrollAbajo = () => {
+        chatEndRef.current.scrollIntoView({
+            block: 'end',
+        });
+    }
 
     return (
         <>
-            {
-                loading ? (<Loader />)
-                    : (
-                        <>
-                            <Navbar showUsers={showUsers} setShowUsers={setShowUsers} />
+            {loading && (<Loader />)}
+            <Navbar showUsers={showUsers} setShowUsers={setShowUsers} />
 
-                            <div className="container-fluid">
-                                <div className="row">
-                                    <ListUsers
-                                        usuarios={usuarios}
-                                        uidChat={uidChat}
-                                        showUsers={showUsers}
-                                        setShowUsers={setShowUsers}
-                                        setUidChat={setUidChat}
-                                    />
+            <div className="container-fluid">
+                <div className="row">
+                    <ListUsers
+                        usuarios={usuarios}
+                        uidChat={uidChat}
+                        showUsers={showUsers}
+                        setShowUsers={setShowUsers}
+                        setUidChat={setUidChat}
+                    />
 
-                                    {/* Columna derecha: Contenido del chat */}
-                                    <div className="col-md-9 col-12 d-flex flex-column">
-                                        <div style={{
-                                            flexGrow: 1,
-                                            overflowY: "auto",
-                                            height: "calc(100vh - 70px)"
-                                        }}
-                                            ref={chatStartRef}
-                                        >
-                                            <ContainerMessages posts={posts} usuarioSesionUid={usuarioSesion.uid} />
-                                            <div ref={chatEndRef} />
-                                        </div>
-                                        {uidChat && (
-                                            <InputSendMessage
-                                                db={db}
-                                                usuarioSesionUid={usuarioSesion.uid}
-                                                uidChat={uidChat}
-                                                setPosts={setPosts}
-                                            />
-                                        )}
-                                    </div>
-                                </div>
-                            </div>
-                        </>
-
-                    )
-            }
+                    {/* Columna derecha: Contenido del chat */}
+                    <div className="col-md-9 col-12 d-flex flex-column">
+                        <div
+                            style={{
+                                flexGrow: 1,
+                                overflowY: "auto",
+                                height: "calc(100vh - 70px)"
+                            }}
+                            ref={chatStartRef}
+                        >
+                            {
+                                uidChat &&
+                                <ContainerMessages
+                                    posts={posts}
+                                    usuarioSesionUid={usuarioSesion.uid}
+                                />
+                            }
+                            <div ref={chatEndRef} />
+                        </div>
+                        {uidChat && (
+                            <InputSendMessage
+                                db={db}
+                                usuarioSesionUid={usuarioSesion.uid}
+                                uidChat={uidChat}
+                                setPosts={setPosts}
+                            />
+                        )}
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
