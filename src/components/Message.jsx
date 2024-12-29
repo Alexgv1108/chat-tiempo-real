e7554107b1e6127562944c8144b501ce5f1081ff
Alexgv1108 <alexgallego1108@gmail.com>
2024-12-29 15:53:57 -0500

    Se agrega componente para cuando no hay internet y se optimizan componentes

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,4 +1,4 @@
-import { memo} from "react";
+import { memo } from "react";
 import { format } from "@formkit/tempo"
 import { AudioMessage } from "./AudioMessage";
 
@@ -7,33 +7,28 @@ const areEqual = (prevProps, nextProps) => {
 };
 
 export const Message = memo(({ postContent, usuarioSesionUid, posts, index, isAnimacionMensaje }) => {
+    const isUserMessage = postContent.uid === usuarioSesionUid;
+    const isLastMessage = index === posts.length - 1 && isAnimacionMensaje;
+
     return (
         <div
-            className={`shadow-sm mb-3 rounded-lg p-4
-                ${postContent.uid === usuarioSesionUid
-                    ? 'bg-gray-600 text-white ml-auto'
-                    : 'bg-gray-100 text-gray-800 mr-auto'
-                }
-                ${index === (posts.length - 1) && isAnimacionMensaje ? 'animate-growFromBottom' : ''}
-              `}
-            style={{
-                maxWidth: "85%",
-            }}
+            className={`shadow-sm mb-3 rounded-lg p-4 max-w-[85%] 
+            ${isUserMessage ? 'bg-gray-600 text-white ml-auto' : 'bg-gray-100 text-gray-800 mr-auto'} 
+            ${isLastMessage ? 'animate-growFromBottom' : ''}`}
         >
-            <div className="flex justify-between items-center">
-                {
-                    postContent.type === 'audio'
-                        ? <AudioMessage 
-                            postContent={postContent}
-                        />
-                        : <p className="text-sm flex-grow">{postContent.mensaje}</p>
-                }
+            <div className="flex justify-between items-start gap-2">
+                {postContent.type === "audio" ? (
+                    <AudioMessage postContent={postContent} />
+                ) : (
+                    <p className="text-sm flex-grow break-words">{postContent.mensaje}</p>
+                )}
 
-                <small className={`${postContent.uid === usuarioSesionUid
-                    ? 'text-white'
-                    : 'text-gray-500'
-                    }`}>{format(new Date(postContent.fecha), { date: "short", time: "medium" })}</small>
+                <small
+                    className={`text-xs self-end ${isUserMessage ? 'text-white' : 'text-gray-500'}`}
+                >
+                    {format(new Date(postContent.fecha), { date: "short", time: "medium" })}
+                </small>
             </div>
         </div>
-    )
+    );
 }, areEqual)
